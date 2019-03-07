3fa9ed036596cf6c75136c0fbca3a2f748dc04b1
CharityQuinn <charityaquinn@gmail.com>
2019-03-06 21:52:34 -0500

    Moved the scrape code out of server and into it's own file in routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const plantRoutes = require("./plantRoutes");
 
 // Plant routes
+router.use("/plants", scrapeRoutes);
 router.use("/plants", plantRoutes);
+
 
 module.exports = router;
