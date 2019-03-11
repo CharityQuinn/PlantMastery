d216b2d1d5dad50382367602fa47b96ffa963e1c
Alpa Patel <alpajp@live.com>
2019-03-11 18:26:36 -0400

    modified Home.js Nav.js Forum.js Plants.js Search.js flowering.js and API.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,29 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Link, Switch} from 'react-router-dom';
 import "./App.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-
+//import Flowering from "./containers/flowering";
+//import Succulents from "./containers/succulents";
+//import Foliage from "./containers/foliage";
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://res.cloudinary.com/q1962/image/upload/v1552069838/sample.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="images/a-fleshy-plant-1494116_640.jpg"
-      alt="First slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="images/Aloe.jpeg"
-      alt="second slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-      <Nav />
-      <Home />
-   
-
+        <Nav />
+        <Switch>
+          <Router exact path="/" component={Home}/>
+          <Router exact path="/" component={Search}/>
+          <Router exact path="/" component={Plants}/>
+          <Router exact path="/" component={Forums}/>
+        </Switch>
       </div>
-    );
+   
+      </Router>
+    )
   }
 }
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,14 +2,19 @@ import React from "react";
 import {Navbar, Button} from 'react-bootstrap'
 import "./Nav.css";
 import {} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+{/* <Link to="/Search Types">Search Types</Link> */}
+
 
 const Nav = () => (
  
 <Navbar  bg="dark" variant="dark">
-  <Navbar.Brand href="/">House Plants Advisor</Navbar.Brand>
-        <Button href="#">Home</Button>
-        <Button href="#">Search Types</Button>
-        <Button href="#">Plantcare</Button>
+  <Navbar.Brand as={Link} to="/">House Plants Advisor</Navbar.Brand>
+        <Button as={Link} to="/">Home</Button>
+        <Button as={Link} to="/search">Search Types</Button>
+        <Button as={Link} to="/plants">Plantcare</Button>
+        <Button as={Link} to="/Forums">Forums</Button>
 </Navbar>
 
 )
