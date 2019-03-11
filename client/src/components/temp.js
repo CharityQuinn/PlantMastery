ea29ef0af9efec3b144f134241244a9c3eea604f
Alpa Patel <alpajp@live.com>
2019-03-11 12:12:06 -0400

    modified App.js, App.css index.html and added more pictures

---
+++ client/src/components/temp.js
@@ -0,0 +1,108 @@
+    <nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">Home</a>
+      <a className="navbar-brand" href="#">Plants Care</a>
+      <a className="navbar-brand" href="#">Forum</a>
+    </nav>
+
+  <div className="container">
+      <div className="profileDiv m-3 row">
+        <div className="col-md-4">
+          <img src="Public/images/Swiss Cheese Plant-leaf" className="profileImg" alt="">
+        </div>
+        <div className="col-md-4">
+          <a className="row" href=""Swiss Cheese Plant</a>
+          <a className="row" href="">RecentBlog :</a>
+          <p>This  is native to the rainforests in south America is a flowering plant, it's primarily grown indoors for its attractive foliage and the height it grows up to. It's a plant that becomes a focal point of a room once it matures. </p>
+          
+        </div>
+        <div className="leaderBoard col-md-4">
+          <p leadText="">Plantcare</p>
+          <p>:</p>
+          <p>A fairly bright room or bright with plenty of shade is best. Direct sunlight will damage the leaves and not enough light can slow growth down. Between watering times allow the soil to become dry to the touch within the top couple of inches or so of soil.A peat based potting soil mix with perlite or sand is ideal. Soil that drains well is needed.</p>
+        </div>
+      </div>
+
+      <div className="profileDiv m-3 row">
+        <div className="col-md-4">
+          <img src="Devils_Ivy" className="" alt="">
+        </div>
+        <div className="col-md-4">
+          <a className="row" href="">Golden Pothos Plant:</a>
+          <a className="row" href="">RecentBlog :</a>
+          <p>This plant is a native of Australia, Indonesia, China, Japan and India. Despite its already wide natural range, it has been imported to locations all over the globe. Although it does wonderfully as a house plant, growing it outdoors is illegal in some states, as it has been declared an invasive species. </p>
+        </div>
+        <div className="leaderBoard col-md-4">
+          <p leadText="">Plantcare</p>
+          <p></p>
+          <p>To keep this plant as healthy as possible, keep it at temperatures between 60-85 ºF (15-29 ºC) year round. It will withstand occasional cold snaps down to 50 ºF (10 ºC), but below this, the plant will die.
+        	This plant does best in partial shade conditions year-round. Too much light will burn the leaves, causing them to lose their marbled quality.	A pot mixture used for cactus plants that drains well will suffice. Adding gravel or small pebbles at the bottom first few inches of the pot will encourage drainage. </p>
+        </div>
+      </div>
+
+      <div className="profileDiv m-3 row">
+        <div className="col-md-4">
+          <img src="Public/images/pachira_money_tree" className=""
+          alt="">
+        </div>
+        <div className="col-md-4">
+          <a className="row" href="">Maxican Fortune or Braided Money Tree: </a>
+          <a className="row" href="">RecentBlog</a>
+          <p>In South America and other warm climates the pachira is grown for it's edible nuts, in east Asia for good fortune, and in Europe and the US for their attractiveness as an indoor tree species. Grown braided as a house plant edible nuts will not fruit.</p>
+        </div>
+        <div className="leaderBoard col-md-4">
+          <p leadText="">Plantcare</p>
+          <p>Sunlight Support</p>
+          <p>Try your best to provide the plant with as much light as possible - without direct sunlight which can scorch the leaves. During the winter the amount of daylight time decreases, which will naturally tell the plant it's time to rest and slow it's growth down, so do not become concerned about it no longer producing new shoots and leaves.A peat moss based mix is suitable. A good mix can be made with 2 parts peat and one part perlite or sand (for drainage).A balanced liquid fertilizer diluted should be fed to the plant every 2 weeks which will encourage growth and healthy leaves. Feed from spring and stop just before winter or when growth really slows down, whichever comes first.</p>
+        </div>
+      </div>
+      <div className="profileDiv m-3 row">
+        <div className="col-md-4">
+          <img src="assets/images/3.jpg" className="profileImg" alt="">
+        </div>
+        <div className="col-md-4">
+          <a className="row" href="">Rescent WO(Abs) : Barbell Rollout, Barbell Russian Twist, Swiss Ball Crunch</a>
+          <a className="row" href="">RecentBlog</a>
+          <p>If you've mastered crunches, you know they work the rectus abdominis and possibly the obliques. But to
+            really hit the muscles deep within your core the transverse abdominis, you need to improve stabilization
+            by recruiting those deeper muscle fibers. As for frequency, your abs should be trained on a regular basis
+            just like any other muscle group.</p>
+        </div>
+        <div className="leaderBoard col-md-4">
+          <p leadText="">Best Post-Workout Nutrients</p>
+          <p> Betaine</p>
+          <p>Anhydrous betaine has been shown to help improve strength, power, and muscular endurance. It does this in part by promoting better hydration as well as fighting cell stress. Utilizing anhydrous betaine in the post-workout window can not only help with recovery, but it will also prepare your body for the next intense workout.</p>
+         
+        </div>
+      </div>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="container">
+            <nav className="navbar mx-auto">
+              <div className="nav mx-auto">
+                <span style="font-size: 3rem;">
+                  <div className="nav-item">
+                    <a className="nav-link fab fa-instagram socIcon" style="color: whitesmoke;" href="https://www.instagram.com"></a>
+                  </div>
+              </div>
+              <div className="nav mx-auto">
+                <span style="font-size: 3rem;">
+                  <div className="nav-item">
+                    <a className="nav-link fab fa-facebook socIcon" style="color: whitesmoke;" href="https://www.facebook.com"></a>
+                  </div>
+              </div>
+              <div className="nav mx-auto">
+                <span style="font-size: 3rem;">
+                  <div className="nav-item">
+                    <a className="nav-link fab fa-twitter-square socIcon" style="color: whitesmoke;" href="https://www.twitter.com"></a>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+  </div>
+
+
+
