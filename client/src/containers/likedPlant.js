90ebd73cfb53845aa6d89900cf36a99b01f665c7
CharityQuinn <charityaquinn@gmail.com>
2019-03-25 10:13:57 -0400

    Inch by inch do I move on likePlant.js to work

--- client/src/containers/likedPlant.js
+++ client/src/containers/likedPlant.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import API from "../utils/API";
+//import API from "../utils/API";
 
 
 class LikedPlant extends Component {
@@ -10,40 +10,48 @@ class LikedPlant extends Component {
 
   componentDidMount() {
     //const plantId = this.props.match.params.plantId;
-    getPlantById();
-    
+    this.getLikedPlant();
   }
 
-  getPlantById = () => {
-    API.getPlantById()
-    .then(res => {
-      let likedGreen = res.data.filter(plant => plant.likedPlant === true);
-      this.setState({
-        likedList: likedGreen
-      })
+  getLikedPlant = () => {
+    let addLiked = localStorage.getItem(storage);
+    this.setState({
+        likedList: addLiked
     })
-    .then(() => this.getLocalStorage())
-    console.log("Inside LikedPlant this is plantdata " + plantData)
-    .catch(err => console.log(err))
-  }
     
-  getLocalStorage = () => {
-    let storage = [];
-    this.state.likedList.map( plant => (
-      localStorage.getItem(plant.name)
-      ? storage.push(plant.name)
-      : ""
-    ))
-    this.setState({localStorage: storage});
+      console.log("Inside LikedPlant this is plantdata " + plant.name)
+    .catch(err => console.log(err))
   }
   
   render() {
     return (
-      <React.Fragment>
-      {Object.keys(this.state.plantData).length ? lchmodSync.stringify(this.state.plantData, null, 2) : ""}
-      
-      </React.Fragment>
-    )
-  }
-}
+      <div className="row" style={{ margin: "0" }}>
+        {this.state.likedList.length ? this.state.likedList.map(plant => (
+          <div className="col-12 col-sm-12 col-md-4 col-lg-3 plantCard mt-4" key={plant._id}>
+            <div className="card"
+              style={{
+              height: "55vh", width: '268px', position: "relative",
+              boxShadow: '10px 5px 20px'
+              }}>
+  
+              <img src={plant.image} alt={plant.name} style={{ height: '160px', boxShadow: '-1px 1px 20px', borderRadius: "20%" }} />
+  
+                <div className="card-body"
+                  style={{ width: '100%', height: '20vh', margin: '2% 0 4% 0', overflowY: 'hidden' }}>
+                  <h5 className="card-title">{plant.name}
+                    {this.isLoved(plant.name)
+                     ? <i className="fas fa-heart float-right" style={{ color: "red" }}></i>
+                     : ""}
+                   </h5>
+                    <p className="card-text" style={{ paddingRight: "5px", height: "6.3rem", overflowY: "auto" }}> {plant.description}</p>
+                </div>
+            </div>
+      </div>
+   
+      )) : ""}
+        </div>
+      )
+    }
+  
+  };
 export default LikedPlant;
