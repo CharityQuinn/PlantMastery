d6500b2197e5a978914d7c8722ecb39c58b20a90
CharityQuinn <charityaquinn@gmail.com>
2019-03-31 13:24:02 -0400

    Working code

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import LikedPlant from "./containers/likedPlant";
+import LikedPlant from "./containers/LikedPlants/LikedPlants";
 import Flowering from "./containers/flowering";
 import Succulents from "./containers/succulents";
 import Foliage from "./containers/foliage";
